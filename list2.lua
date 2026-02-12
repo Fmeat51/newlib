df375a5e994df4fe56676710b011a49b1c2886c5
Venus Ring <Fmeat51@gmail.com>
2026-02-11 22:59:39 -0300

    Create list2.lua

---
+++ list2.lua
@@ -0,0 +1,11 @@
+return {
+  CraftTurbo46489 = 'Owner',
+  --// flower hosts
+  vwlzxz = 'Autokick',
+  kasumori = 'Autokick',
+  bogdan12312312312312 = 'Autokick',
+  SetroBoomin = 'Autokick',
+  SimplyJayden = 'Autokick',
+  AutomaticSpammer = 'Autokick',
+  Qrusheds = 'Autokick'
+}
